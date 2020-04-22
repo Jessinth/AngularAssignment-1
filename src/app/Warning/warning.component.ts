1188186b848c1f131584f52a8930a05427b94fdb
Priyadarshan <mail2jessinth@gmail.com>
2020-04-22 11:31:28 +0530

    Added Assignment 1

---
+++ src/app/Warning/warning.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:'app-warning',
+    template:'<h3> Warning!!! </h3>',
+    styles : ['h3{ color:red;  }']
+})
+
+export class WarningComponent {
+   
+}
